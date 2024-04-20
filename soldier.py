260895df808fa2d7abbc9124a2e9e30f01feec93
rugoba89 <rubegb89@gmail.com>
2024-04-20 14:59:53 +0200

    calse soldier.py ok

--- soldier.py
+++ soldier.py
@@ -1,11 +1,21 @@
 import random
 
 class Soldier:
-    def __init__(self, health, strength):
-        print("Soldier created")
+    def __init__(self, health: int, strength: int):
+        self.health = health
+        self.strength = strength
+        self.isAlive = True
 
     def attack(self):
-        print("Soldier attacked")
+        if self.isAlive:
+            return self.strength
+
 
     def receiveDamage(self, damage):
-        print("Soldier received damage")
+        if self.health <= 0:
+            self.isAlive = False
+            return
+
+        self.health = self.health - damage
+
+
