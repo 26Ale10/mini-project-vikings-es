c071989d66b3dc31a6afab43762b05265273daf6
ObedSAGA <msanchezobed@gmail.com>
2024-04-20 15:10:09 +0200

    Add vikings class

--- viking.py
+++ viking.py
@@ -1,12 +1,21 @@
-import random
+
 from soldier import Soldier
 
 class Viking(Soldier):
     def __init__(self, name, health, strength):
+        super().__init__(health, strength)
+        self.name = name
         print('Viking created')
 
-    def battleCry(self):
-        print('Odin Owns You All!')
+    def attack() ->str:
+        return "Odin Owns You All!"
+    
+    def battleCry(self) -> str:
+        return '¡Odin os posee a todos!'
 
-    def receiveDamage(self, damage):
-        print('Viking received damage')
+    def receiveDamage(self, damage) -> str:
+        self.health - damage
+        if self.health <= 0:
+            return f"{self.name} ha muerto en acto de combate"
+        else:
+            return f"{self.name} ha recibido PUNTOS DE DAÑO"
