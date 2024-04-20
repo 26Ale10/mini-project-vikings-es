d1a065acfa6ffc1a88355dfaf3c929fa96e2310d
26Ale10 <villegas261093@gmail.com>
2024-04-20 15:16:28 +0200

    Subiendo fichero Saxon

--- saxon.py
+++ saxon.py
@@ -1,10 +1,21 @@
 import random
+
 from soldier import Soldier
+# Aquí estuvo Alejandro
+
 
 class Saxon(Soldier):
     def __init__(self, health, strength):
+        super().__init__(health, strength)
         print('Saxon created')
 
-    def receiveDamage(self, damage):
-        print('Saxon received damage')
+    def attack(self):
+        return self.strength
 
+    def receiveDamage(self, damage):
+        # self.damage = damage
+        self.health = self.health - damage
+        if self.health >= 0:
+            print("Un 'Saxon' ha recibido PUNTOS DE DAÑO puntos de daño")
+        else:
+            print("Un 'Saxon' ha muerto en combate")
